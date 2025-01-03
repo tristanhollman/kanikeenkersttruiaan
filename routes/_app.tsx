2b5947d9c761a268a8dd7c9cff276ad43f26d42b
Tristan Hollman <tristanhollman@users.noreply.github.com>
2025-01-03 12:59:55 +0100

    Add favicon to website (#7)
    Fixes #6
    
    Add favicon link to the website
    
    * Add a `<link>` element in the `<head>` section of `routes/_app.tsx` to reference the `static/logo.svg` file as a favicon
    * Set the `rel` attribute to `icon` and the `href` attribute to `/logo.svg` for the favicon link
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/tristanhollman/kanikeenkersttruiaan/issues/6?shareId=XXXX-XXXX-XXXX-XXXX).

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>kanikeenkersttruiaan</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="icon" href="/logo.svg" />
       </head>
       <body>
         <Component />
